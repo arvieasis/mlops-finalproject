1dac77a7ae8bc6497a7f95c89c88ad43e1400a69
Samn Mercado <samnmercado@gmail.com>
2025-03-08 16:42:46 +0800

    Create imports.py

---
+++ assets/imports.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report
